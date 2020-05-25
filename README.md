6f3581d4362ceb8d409d460cbe8e9337a8cc8e6d
harshthorwat <harshthorwat.dtis@gmail.com>
2020-05-25 10:17:18 +0530

    Change in readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # html2pdf
 Bash script to convert html to pdf.
+
+Use following commands
+
+> chmod +x html2pdf.sh
+> ./html2pdf.sh
--- html2pdf.sh
+++ html2pdf.sh
@@ -8,7 +8,7 @@ then
 	bold=$(tput bold)
 	normal=$(tput sgr0)
 	echo "Enter HTML file name ${bold}without extension${normal}. (example.html --> example)"
-	echo "If file is not in same folder then specify full path. (/home/user/html/example)"
+	echo "If file is ${bold}not in same folder ${normal}then specify full path. (/home/user/html/example)"
 	read filename
 	file="${filename}.html"
 	out="${filename}.pdf"
